e97274bea4df4ac535134771b3fe8e2b40b214d7
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-02-22 10:27:15 +0100

    force pkg config

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 set -ex
 export LIBGIT2_NO_VENDOR=true
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
 cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
 cargo install --path git-cliff --bins --root "${PREFIX}" --locked
 
