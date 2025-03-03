029192f85ee87dcda938d0bed04a0471dfdb4049
Hadrien Mary <hadrien.mary@gmail.com>
2025-03-03 14:01:28 -0500

    bump to 2.8.0

--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-export LIBGIT2_NO_VENDOR=true
-export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
-
-cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
-cargo install --path git-cliff --bins --root "${PREFIX}" --locked
-
-rm $PREFIX/.crates2.json
-rm $PREFIX/.crates.toml
